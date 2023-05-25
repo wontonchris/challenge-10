22611cd2277907311280069b4bb7a29ffcdb45f9
wontonchris <127700194+wontonchris@users.noreply.github.com>
2023-05-25 16:57:55 -0500

    working babygit statusgit status

--- index.js
+++ index.js
@@ -1,37 +1,72 @@
 // Import modules
-const inquire = require('inquirer');
-const {Circle, Square, Triangle} = require('./lib/shapes');
+const inquirer = require('inquirer');
+const { Circle, Square, Triangle } = require('./lib/shapes');
 const fs = require('fs');
 const questions = require('./lib/questions');
 
 // Define SVG class
-class Svg{
-    constructor(){
-        this.textElement = ''
-        this.shapeElement = ''
+class Svg {
+    constructor() {
+      this.textElement = '';
+      this.shapeElement = '';
     }
-    render(){
+  
+    render() {
+      return `<svg version="1.0" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
+    }
+  
+    setTextElement(text, color) {
+      this.textElement = `<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="60" fill="${color}">${text}</text>`;
+    }
+  
+    setShapeElement(shape, color) {
+      this.shapeElement = `<${shape} cx="50%" cy="50%" r="100" height="100%" width="100%" fill="${color}"></${shape}>`;
+    }
+  }
+  
 
-        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
-    }
-    setTextElement(text,color){
-        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
-    }
-    setShapeElement(shape){
-        this.shapeElement = shape.render()
 
-    }
-    
-}
-
+  
 
 // write data function
 function writeToFile(fileName, data) {
-    console.log(data)
-    console.log(fileName)
-    fs.writeFile(fileName, data, (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-        });
-    }
+  fs.writeFile(fileName, data, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
+}
 
+// init function
+async function init() {
+  console.log('SVG Logo Generator!');
+  const svgFile = 'logo.svg';
+
+  // answers from questions
+  const answers = await inquirer.prompt(questions);
+
+  // user input validation
+  if (answers.text.length > 0 && answers.text.length <= 3) {
+    console.log('Text:', answers.text);
+  } else {
+    console.log('Invalid number of characters');
+    return;
+  }
+
+  // user answers
+  const user_text = answers.text;
+  const user_font_color = answers.textColor;
+  const user_shape_color = answers.shapeColor;
+  const user_shape = answers.shape;
+
+  // SVG Logo Generator
+  const svg = new Svg();
+  svg.setTextElement(user_text, user_font_color);
+  svg.setShapeElement(user_shape, user_shape_color);
+  const svgString = svg.render();
+
+  // write to file
+  writeToFile(svgFile, svgString);
+}
+
+// call init function
+init();
