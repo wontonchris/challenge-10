3d99dd89fcf47f3cf6e2cbfbc4e4a84cc22fe5dc
wontonchris <127700194+wontonchris@users.noreply.github.com>
2023-05-25 11:16:05 -0500

    modified index fetch or async idk

--- index.js
+++ index.js
@@ -5,12 +5,33 @@ const fs = require('fs');
 const questions = require('./lib/questions');
 
 // Define SVG class
-class SVG{
+class Svg{
     constructor(){
-        this.textElements = [];
-        this.shapeElements = [];
+        this.textElement = ''
+        this.shapeElement = ''
+    }
+    render(){
+
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+    }
+    setTextElement(text,color){
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    }
+    setShapeElement(shape){
+        this.shapeElement = shape.render()
+
+    }
+    
 }
 
-// Render SVG
-render(){
-    return `
+
+// write data function
+function writeToFile(fileName, data) {
+    console.log(data)
+    console.log(fileName)
+    fs.writeFile(fileName, data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+        });
+    }
+
